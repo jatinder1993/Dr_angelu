92b21dc5aeb9e14c52ac8b887a12df9db0528410
jatinder1993 <gill09g1@gmail.com>
2023-10-14 10:20:44 -0400

    test wind1

--- test_sept_2023.py
+++ test_sept_2023.py
@@ -1,5 +1,12 @@
-F = "Ferrari"
-print(F)
-test = "Windows"
-print(test)
-print(F)
+class Load:
+    def __init__(self):
+        self.invoice = input("Enter the invoice number:  ")
+        self.price = float(input("Enter the Amount in numbers only:  "))
+        self.start_odo = int(input("Enter the trip start Odometer:  "))
+        self.end_odo = int(input("Enter the end of trip odometer:  "))
+        self.broker = input("Enter the name of broker:  ")
+
+    def get_info(self):
+        print(f'Invoice {self.invoice}, Price = {self.price}')
+
+Load()
